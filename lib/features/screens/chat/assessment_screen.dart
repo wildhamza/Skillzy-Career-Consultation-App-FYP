5e26a4ba4def47a3f4dcb66bb40b96ad70b621d9
Ghayas <ghayasleo99@gmail.com>
2025-01-03 00:56:02 +0500

    Added dark mode and remove app bar from authentication pages

---
+++ lib/features/screens/chat/assessment_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xffC4DFCB),
+      child: Center(
+        child: Text(
+          "Chat",
+          style: TextStyle(
+            color: Colors.green[900],
+            fontSize: 45,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
