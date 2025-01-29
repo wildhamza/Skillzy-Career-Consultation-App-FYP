625c9fe5aa1b4bfa8687c527490f528b6e50b6b4
Ghayas <ghayasleo99@gmail.com>
2025-01-29 21:31:08 +0500

    Added all questions, created edit profile and change password screen

---
+++ lib/features/screens/edit_profile/edit_profile_screen.dart
@@ -0,0 +1,65 @@
+import 'package:basics/components/setting_menu_button.dart';
+import 'package:basics/utils/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class EditProfile extends StatelessWidget {
+  const EditProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Edit Profile", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+          backgroundColor: Colors.transparent,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const SizedBox(height: 32),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'First Name',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Last Name Input Field
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Last Name',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Spacer(),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Get.offNamed('/home');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppTheme.primaryColor[500],
+                          foregroundColor: Colors.white,
+                        ),
+                        child: Text(
+                          'Update Profile'.toUpperCase(),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
